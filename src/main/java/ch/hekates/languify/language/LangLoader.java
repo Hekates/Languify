1f20e87dc9b2b9aed63096fd3b2cf501f46acce9
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-06-10 19:48:19 +0200

    hopefully fixed it

--- src/main/java/ch/hekates/languify/language/LangLoader.java
+++ src/main/java/ch/hekates/languify/language/LangLoader.java
@@ -28,16 +28,16 @@ public class LangLoader {
 
         if (file.exists()) {
             log.info("The specified language file is located in the right directory... proceeding.");
+            plugin.saveResource("lang\\" + language + ".json", true);
             for (File language_file : plugin.getDataFolder().listFiles()){
                 plugin.saveResource(language_file.getName(), true);
             }
-            plugin.saveResource("lang\\" + language + ".json", true);
         } else {
             log.warning("The specified language file is not located in the right directory... generating a new one.");
+            plugin.saveResource("lang\\" + language + ".json", true);
             for (File language_file : plugin.getDataFolder().listFiles()){
                 plugin.saveResource(language_file.getName(), true);
             }
-            plugin.saveResource("lang\\" + language + ".json", true);
         }
     }
 }
