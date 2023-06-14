a68b8592045dfac06b53ea5443219f8833c63c87
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-06-14 08:41:46 +0200

    fixed language lader

--- src/main/java/ch/hekates/languify/language/LangLoader.java
+++ src/main/java/ch/hekates/languify/language/LangLoader.java
@@ -1,33 +1,48 @@
 package ch.hekates.languify.language;
 
 import ch.hekates.languify.Languify;
-import com.sun.tools.javac.Main;
 import org.bukkit.plugin.Plugin;
 
-import java.io.File;
-import java.util.Arrays;
-import java.util.logging.Logger;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Collections;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class LangLoader {
     /**
      * This method loads the set language by checking if the file already exists. If not, it clones the file out of the plugin jar file into the language folder.
+     *
      * @param language The language file name without the file ending .json.
      * @see Languify#setFileDirectory(String)
      */
+    private static final Plugin plugin = Languify.getPlugin();
+
     public static void loadLanguage(String language) {
         Languify.setLanguage(language);
     }
 
-    public static void saveLanguages(){
-        Plugin plugin = Languify.getPlugin();
+    public static void saveLanguages(String pluginName, String version) {
+        File targetDirectory = new File("plugins/" + pluginName);
+        String jarFilePath = "plugins/" + pluginName + version + ".jar";
 
-        File langFolder = new File(plugin.getDataFolder(), "lang");
-        File[] files = langFolder.listFiles();
+        try (ZipFile jarFile = new ZipFile(jarFilePath)) {
+            for (ZipEntry entry : Collections.list(jarFile.entries())) {
+                if (entry.getName().startsWith("lang/") && !entry.isDirectory()) {
+                    String targetFilePath = targetDirectory.getPath() + "/" + entry.getName();
 
-        for (File file : files) {
-            if (file.isFile()) {
-                plugin.saveResource("lang/" + file.getName(), true);
+                    File targetFile = new File(targetFilePath);
+                    targetFile.getParentFile().mkdirs();
+
+                    try (InputStream inputStream = jarFile.getInputStream(entry)) {
+                        Files.copy(inputStream, targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    }
+                }
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
